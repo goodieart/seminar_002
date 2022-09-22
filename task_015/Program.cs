2b885f54f0983a5bc8ce87997eed833580c27969
Pavel Yastremsky <goodieart@gmail.com>
2022-09-22 15:44:08 +0300

    task_015 completed

---
+++ task_015/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+Write("Введите номер дня недели: ");
+int day = int.Parse(ReadLine()!);
+
+if(day > 0 & day < 8)
+{
+    string prefix = day == 6 | day == 7?"":"не ";
+    WriteLine($"Это {prefix}выходной день");
+}
+else WriteLine("Введен неверный номер дня недели");
